3082007f851f3e59fd13ba4be6dde5e54d7cace9
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-02-19 17:05:24 +0900

    Add: vue.jsをインストールしました。

---
+++ app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello vue3!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
