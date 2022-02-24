d8f04c69636ce4847d472398972c14c963e7e5a3
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-02-24 21:54:33 +0900

    Add: 大喜利のお題を非同期で投稿する機能を実装

--- app/javascript/pages/title/index.vue
+++ app/javascript/pages/title/index.vue
@@ -3,52 +3,58 @@
     <transition name="fade" appear>
       <p>{{ message }}</p>
     </transition>
-    <button @click="increment">UP</button>
-    <p>{{ $store.state.count }}</p>
-    <p>タイトル一覧</p>
     <ul>
       <li v-for="title in titles" :key="title.index">
         {{ title.id }} - {{ title.theme }} - {{ title.user_name }}
       </li>
     </ul>
-    <p>リプライ一覧</p>
-    <ul>
-      <li v-for="reply in replies" :key="reply.index">
-        {{ reply.id }} - {{ reply.reply_title }} - {{ reply.user_name }}
-      </li>
-    </ul>
+    <label for="theme">タイトル</label>
+    <input type="text" id="theme" v-model="newTitle.theme">
+    <br>
+    <label for="user_name">ユーザー名</label>
+    <input type="text" id="user_name" v-model="newTitle.user_name">
+    <br>
+    <button
+    @click="handleCreateNewTitle"
+    >お題を投稿</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'TitleIndex',
   data() {
     return {
-      message: 'This is Title Index Page!',
-      titles: [],
-      replies: []
+      message: 'お題一覧',
+      replies: [],
+      newTitle: {
+        user_name: '',
+        theme: ''
+      }
     }
   },
+  computed: {
+    ...mapGetters(['titles'])
+  },
   methods: {
-    increment() {
-      this.$store.dispatch('incrementActions')
-    },
-    fetchTitles() {
-      axios.get("api/titles")
-        .then(res => this.titles = res.data)
-        .catch(err => console.log(err.status));
-    },
+    ...mapActions(['createNewTitle', 'fetchTitles']),
     fetchReplies() {
       axios.get("api/titles/1/replies")
         .then(res => this.replies = res.data)
         .catch(err => console.log(err.status));
     },
+    async handleCreateNewTitle() {
+      try{
+        await this.createNewTitle(this.newTitle);
+      } catch(error) {
+        console.log(error)
+      }
+    }
   },
   created() {
     this.fetchTitles();
-    this.fetchReplies();
   }
 }
 </script>
@@ -57,6 +63,10 @@ export default {
 
 #title-index {
   background-color: #bccfc9;
+}
+
+ul {
+  list-style: none;
 }
 
 </style>
--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -1,20 +1,43 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export const store = createStore({
   state () {
     return {
-      count: 0,
-      character: 'Gabiyama is genius!'
+      titles: []
     }
   },
+  getters: {
+    titles: state => state.titles
+  },
   mutations: {
-    increment (state) {
-      state.count++
+    setTitles: (state, titles) => {
+      state.titles = titles
+    },
+    addTitle: (state, title) => {
+      state.titles.push(title)
     }
   },
   actions: {
-    incrementActions(context) {
-      context.commit('increment')
-    }
+    fetchTitles({ commit }) {
+      return axios.get('api/titles')
+        .then(res => {
+          commit('setTitles', res.data)
+        })
+        .catch(err => {
+          console.log(err.response)
+          alert('通信に失敗しました。インターネットが繋がっているか確認し、再度実行してください。')
+        })
+    },
+    createNewTitle({ commit }, title) {
+      return axios.post('api/titles', title)
+        .then(res => {
+          commit('addTitle', res.data)
+        })
+        .catch(err => {
+          console.log(err.response)
+          alert('通信に失敗しました。インターネットが繋がっているか確認し、再度実行してください。')
+        })
+    },
   }
 })
