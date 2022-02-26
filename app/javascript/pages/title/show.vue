00b55c83c83744feb5b0176c6296e757fbb29da0
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-02-27 02:58:03 +0900

    Add: お題の回答画面を実装

--- app/javascript/pages/title/show.vue
+++ app/javascript/pages/title/show.vue
@@ -1,19 +1,63 @@
 <template>
   <div id="title-show">
-    <transition name="fade" appear>
-      <p>{{ message }}</p>
-    </transition>
+    <h3>タイトル詳細</h3>
+    <div class="container">
+      <router-link :to="{name: 'TitleIndex'}">お題一覧へ</router-link>
+      <h4>お題 : {{ selectedTitle[0].theme }}
+        <small>投稿者 : {{ selectedTitle[0].user_name }}</small>
+      </h4>
+        <ul>
+          <li
+            v-for="(reply, index) in replies" :data-index="index"
+            :key="reply">
+            {{ reply.id }} . {{ reply.reply_title }} - {{ reply.user_name }}
+          </li>
+        </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { mapGetters } from 'vuex'
 export default {
   name: 'TitleShow',
   data() {
     return {
-      message: 'This is Title Show Page!'
+      newReplay: {
+        title_id: null,
+        newReplay_title: '',
+        user_name: '',
+        favorite: 0,
+      }
     }
+  },
+  computed: {
+    ...mapGetters(['titles', 'replies']),
+    selectedTitle() {
+      return this.titles.filter(title => {
+        return title.id == this.$route.params.id;
+      })
+    }
+  },
+  methods: {
+    getReplies() {
+      axios.get(
+        `/api/titles/${this.$route.params.id}/replies`
+      )
+        .then(res => {
+          this.$store.commit('setReplies', res.data)
+        })
+        .catch(err => {
+          console.log(err.response)
+          alert('通信に失敗しました。インターネットが繋がっているか確認し、再度実行してください。')
+        })
+    }
+  },
+  created() {
+    this.getReplies();
   }
+
 }
 </script>
 
@@ -21,6 +65,12 @@ export default {
 
 #title-show {
   background-color: #d0d2d1;
+}
+
+.container {
+  width: 60%;
+  margin-left: 50px;
+  min-height: 100vh;
 }
 
 </style>
