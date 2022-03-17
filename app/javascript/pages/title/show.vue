801532aa1b4b1a0b2c33a0112a248bc51181a0f4
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-03-18 08:07:58 +0900

    Improve: 解答一覧の初期配置を「いいねの多い順」に変更

--- app/javascript/pages/title/show.vue
+++ app/javascript/pages/title/show.vue
@@ -1,87 +1,16 @@
 <template>
   <div id="title-show">
     <div class="container mx-auto h-5/6 w-11/12 p-3 m-3 bg-dark">
-      <h1 class="m-3 p-2 bg-orange-default text-dark">お題 「 {{ selectedTitle.theme }} 」
+      <h1 class="m-2 p-2 bg-orange-default text-dark">お題 「 {{ selectedTitle.theme }} 」
       </h1>
-      <div class="flex justify-around">
-        <form class="container mx-auto xl:flex absolute bottom-0 left-0 w-screen">
-          <div class="sm:flex mb-3 xl:mb-0 flex-1 justify-around">
-            <div class="sm:flex sm:items-center">
-              <div>
-                <label
-                  class="block font-bold sm:text-right mb-1 sm:mb-0 pr-4"
-                  for="inline-theme">
-                  回答
-                </label>
-              </div>
-              <div>
-                <input
-                  class="focus:ring-2 focus:ring-orange-default appearance-none py-2 px-4 mx-4 text-dark leading-tight focus:outline-none"
-                  id="inline-theme"
-                  type="text"
-                  name="reply_title"
-                  v-model="newReply.reply_title">
-              </div>
-            </div>
-            <div class="sm:flex sm:items-center">
-              <div>
-                <label
-                  class="block font-bold sm:text-right mb-1 sm:mb-0 pr-4" for="inline-user-name">
-                  お名前
-                </label>
-              </div>
-              <div>
-                <input
-                  class="focus:ring-2 focus:ring-orange-default appearance-none py-2 px-4 mx-4 text-dark leading-tight focus:outline-none"
-                  id="inline-user-name"
-                  type="text"
-                  name="user_name"
-                  v-model="newReply.user_name">
-              </div>
-            </div>
-          </div>
-          <div class="sm:flex mb-0 flex-1 justify-around">
-            <div class="sm:flex sm:items-center">
-              <button
-                class="text-dark shadow bg-orange-default py-2 px-6 w-full"
-                type="button"
-                @click="showDrawingSpace"
-              >
-                絵を描く
-              </button>
-            </div>
-            <div class="sm:flex mb-2 sm:mb-0">
-              <div class="relative text-dark bg-orange-default flex justify-center items-center hover:cursor-pointer w-5/6">
-                <div class="absolute">
-                  <div class="flex flex-col items-center ">
-                    <span class="block font-normal">絵を選択</span>
-                  </div>
-                </div>
-                <input
-                  id="image"
-                  type="file"
-                  name="image"
-                  class="opacity-0"
-                  @change="handleChange"
-                >
-              </div>
-            </div>
-            <div class="sm:flex sm:items-center">
-              <button
-                class="text-dark shadow bg-orange-default py-2 px-6 w-full" type="button"
-                @click="handleCreateNewReply">
-                回答
-              </button>
-            </div>
-          </div>
-        </form>
+      <div class="flex m-2">
         <ul
-          class="flex-1 flex text-dark overflow-auto h-96 p-3"
+          class="flex text-dark overflow-auto h-96"
         >
           <li
-            v-for="(reply, index) in filteredReplies" :data-index="index"
+            v-for="(reply, index) in ranking" :data-index="index"
             :key="reply"
-            class="bg-orange-default mr-2 w-40 flex flex-col justify-between"
+            class="bg-orange-default w-40 mr-2 flex flex-col justify-between"
           >
             <div class="self-center bg-white w-40 h-32">
               <img
@@ -99,10 +28,10 @@
                 @click="handleUpdateReply(reply)"
                 class="m-2 p-2 bg-dark text-orange-default"
               >{{ reply.favorite }} <small>いいね！</small></button>
-              <button
+              <!-- <button
                 class="m-2 p-2 bg-dark text-orange-default"
                 @click="handleDeleteReply(reply)"
-              >削除</button>
+              >削除</button> -->
             </div>
           </li>
           <li v-show="filteredReplies.length == 0">
@@ -110,19 +39,82 @@
           </li>
         </ul>
       </div>
-      <div class="container">
-        <router-link
-          class="text-sm py-1 px-2 m-3 bg-orange-default text-dark"
-          :to="{ name: 'Titles' }"
-        >
-          お題一覧へ戻る
-        </router-link>
-      </div>
-    <DrawingSpace
-      v-if="isVisibleDrawingSpace"
-      :currentTheme="selectedTitle.theme"
-      @close-drawing-space="closeModal"
-    />
+      <form class="container mx-auto xl:flex absolute bottom-0 left-0 w-screen">
+        <div class="sm:flex mb-3 xl:mb-0 flex-1 justify-around">
+          <div class="sm:flex sm:items-center">
+            <div>
+              <label
+                class="block font-bold sm:text-right mb-1 sm:mb-0 pr-4"
+                for="inline-theme">
+                回答
+              </label>
+            </div>
+            <div>
+              <input
+                class="focus:ring-2 focus:ring-orange-default appearance-none py-2 px-4 mx-4 text-dark leading-tight focus:outline-none"
+                id="inline-theme"
+                type="text"
+                name="reply_title"
+                v-model="newReply.reply_title">
+            </div>
+          </div>
+          <div class="sm:flex sm:items-center">
+            <div>
+              <label
+                class="block font-bold sm:text-right mb-1 sm:mb-0 pr-4" for="inline-user-name">
+                お名前
+              </label>
+            </div>
+            <div>
+              <input
+                class="focus:ring-2 focus:ring-orange-default appearance-none py-2 px-4 mx-4 text-dark leading-tight focus:outline-none"
+                id="inline-user-name"
+                type="text"
+                name="user_name"
+                v-model="newReply.user_name">
+            </div>
+          </div>
+        </div>
+        <div class="sm:flex mb-0 flex-1 justify-around">
+          <div class="sm:flex sm:items-center">
+            <button
+              class="text-dark shadow bg-orange-default py-2 px-6 w-full"
+              type="button"
+              @click="showDrawingSpace"
+            >
+              絵を描く
+            </button>
+          </div>
+          <div class="sm:flex mb-2 sm:mb-0">
+            <div class="relative text-dark bg-orange-default flex justify-center items-center hover:cursor-pointer w-5/6">
+              <div class="absolute">
+                <div class="flex flex-col items-center ">
+                  <span class="block font-normal">絵を選択</span>
+                </div>
+              </div>
+              <input
+                id="image"
+                type="file"
+                name="image"
+                class="opacity-0"
+                @change="handleChange"
+              >
+            </div>
+          </div>
+          <div class="sm:flex sm:items-center">
+            <button
+              class="text-dark shadow bg-orange-default py-2 px-6 w-full" type="button"
+              @click="handleCreateNewReply">
+              回答
+            </button>
+          </div>
+        </div>
+      </form>
+      <DrawingSpace
+        v-if="isVisibleDrawingSpace"
+        :currentTheme="selectedTitle.theme"
+        @close-drawing-space="closeModal"
+      />
     </div>
   </div>
 </template>
