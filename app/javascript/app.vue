3647633a98a9f5747659e80421e9e2450a9689f8
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-03-19 15:16:31 +0900

    Add: お題と回答投稿時に、フラッシュメッセージが表示される機能を実装

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,16 +1,27 @@
 <template>
-  <div id="app" class="font-ReggaeOne text-orange-default h-screen w-screen bg-dark-lighter flex">
-    <Sidebar v-if="this.$route.name != 'Top'" class="container min-w-52 w-52 flex-none bg-dark-default"/>
-    <router-view class="relative w-full"/>
+  <div id="app" class="font-ReggaeOne text-orange-default h-screen w-screen bg-dark-default flex">
+    <Sidebar v-if="this.$route.name != 'Top'"
+      class="container min-w-52 w-52 flex-none bg-dark-default"
+    />
+    <div
+      class="relative w-full flex flec-col"
+    >
+      <Message
+        v-if="this.$route.name != 'Top'"
+      />
+      <router-view class="relative w-full h-full"/>
+    </div>
   </div>
 </template>
 
 <script>
 import Sidebar from './components/Sidebar.vue'
+import Message from './components/Message.vue'
 export default {
   name: 'App',
   components: {
-    Sidebar
+    Sidebar,
+    Message
   }
 }
 </script>
