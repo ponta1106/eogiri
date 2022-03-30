52a77b0106ea464d52bc733e772eaa7757481814
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-03-30 12:21:25 +0900

    Improve: Webpacker.ymlを修正

--- app/javascript/components/Message.vue
+++ app/javascript/components/Message.vue
@@ -4,11 +4,11 @@
     v-show="content"
     class="bg-white text-dark-default absolute bottom-10 left-0 w-5/6 container z-10 rounded filter drop-shadow flex justify-between"
   >
-    <!-- <img
+    <img
       src="../stylesheets/images/logo.png"
       alt="logo"
       class="h-20 rounded-l"
-    > -->
+    >
     <p class="flex-1 text-center self-center">{{ content }}</p>
   </div>
 </template>
--- app/javascript/pages/top/top.vue
+++ app/javascript/pages/top/top.vue
@@ -2,7 +2,7 @@
   <div id="top-page">
     <div class="h-full flex justify-around bg-dark-default">
       <div class="flex flex-col container self-center h-11/12 w-11/12">
-        <!-- <img src="../../stylesheets/images/logo.png" class="h-96 w-96 mx-auto"> -->
+        <img src="../../stylesheets/images/logo.png" class="h-96 w-96 mx-auto">
         <button class="hover:bg-orange-default hover:text-dark-default border-2 border-orange-default font-bold text-center py-2 px-6 w-1/3 mx-auto" type="button">
         <router-link to="/titles">はじめる</router-link>
         </button>
