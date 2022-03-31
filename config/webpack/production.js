4417482841b8253103a4fd8d679a8d4163e66bb9
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-03-31 16:11:23 +0900

    Fix: `tailwind`を一旦削除

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,28 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const environment = require('./environment')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-
-function hotfixPostcssLoaderConfig (subloader) {
-  const subloaderName = subloader.loader
-  if (subloaderName === 'postcss-loader') {
-    if (subloader.options.postcssOptions) {
-      console.log(
-        '\x1b[31m%s\x1b[0m',
-        'Remove postcssOptions workaround in config/webpack/environment.js'
-      )
-    } else {
-      subloader.options.postcssOptions = subloader.options.config;
-      delete subloader.options.config;
-    }
-  }
-}
-
-environment.loaders.keys().forEach(loaderName => {
-  const loader = environment.loaders.get(loaderName);
-  loader.use.forEach(hotfixPostcssLoaderConfig);
-});
+module.exports = environment.toWebpackConfig()
