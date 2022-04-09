28dff7b8591284276d965e33e4eaeb987839e4f9
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-04-09 13:02:07 +0900

    Improve: Vueテンプレートのエントリーポイントをapplication.jsに変更

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue'
 import App from '../app.vue'
 import router from '../router'
 import { store } from "../store";
-// import "../stylesheets/dist.css";
+import "../stylesheets/dist.css";
 // import "../stylesheets/style.css";
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = createApp(App)
-    .use(router)
-    .use(store)
-  app.mount('#vue-app');
+  const selector = '#vue-app';
+  if(document.querySelector(selector)){
+    createApp(App).use(router).use(store).mount(selector);
+  }
 })
