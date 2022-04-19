c82e850061f98ab6e7d2bf86ad005c00bd0e14e4
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-04-20 03:14:40 +0900

    Improve: 回答一覧画面のレイアウトを修正

--- app/javascript/pages/top/top.vue
+++ app/javascript/pages/top/top.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="top-page">
-    <div class="h-full flex justify-around bg-dark-default">
+    <div class="h-screen flex justify-around bg-dark-default">
       <div class="flex flex-col container self-center h-11/12 w-11/12">
         <img src="../../stylesheets/images/logo.png" class="h-96 w-96 mx-auto">
         <button class="hover:bg-orange-default hover:text-dark-default border-2 border-orange-default font-bold text-center py-2 px-6 w-1/3 mx-auto" type="button">
