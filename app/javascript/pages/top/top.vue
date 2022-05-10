fcea902e9a1fa6578d90502ad9024710cbd2e997
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-05-11 04:13:03 +0900

    Test: はじめるボタンの増加テスト

--- app/javascript/pages/top/top.vue
+++ app/javascript/pages/top/top.vue
@@ -5,6 +5,9 @@
         <img src="../../stylesheets/images/logo.png" class="h-96 w-96 mx-auto">
         <button class="hover:bg-orange-default hover:text-dark-default border-2 border-orange-default font-bold text-center py-2 px-6 w-1/3 mx-auto" type="button">
         <router-link to="/titles">はじめる</router-link>
+        <router-link to="/titles">はじめる</router-link>
+        <router-link to="/titles">はじめる</router-link>
+        <router-link to="/titles">はじめる</router-link>
         </button>
       </div>
     </div>
