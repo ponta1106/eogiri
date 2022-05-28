0df0f12ecc7fdc6791a6a45d8a32dcfccea38005
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-05-28 17:40:07 +0900

    Improve: 作者アイコンのサイズを修正、Twitterリンクボタンのデザインを修正

--- app/javascript/pages/contact/index.vue
+++ app/javascript/pages/contact/index.vue
@@ -6,7 +6,7 @@
         <div class="adimin-icon">
           <img
             src="../../assets/turtle.png" alt="icon"
-            class="w-1/3 mx-auto">
+            class="h-24 mx-auto">
         </div>
         <div class="message mb-5">
           <p>こんにちは、このサイトの管理人ぽんたです。</p>
@@ -14,10 +14,9 @@
         </div>
         <div class="contact">
           <button
-            class="my-2 py-2 px-4 bg-orange-default text-dark-default"
+            class="my-2 py-2 px-4 bg-blue-400 text-white rounded"
             @click="contactAdmin"
           >
-            <!-- <font-awesome-icon :icon="['fab', 'twitter']" /> -->
             管理人のTwitter
           </button>
         </div>
