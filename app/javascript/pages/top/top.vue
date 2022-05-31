838aaab8cd26449f75ab48a6397c5bc1d32ead07
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2022-05-31 10:43:58 +0900

    Fix: はじめるボタンが押せないバグを修正

--- app/javascript/pages/top/top.vue
+++ app/javascript/pages/top/top.vue
@@ -3,12 +3,14 @@
     <div class="h-screen flex justify-around bg-dark-default md:-ml-44">
       <div class="flex flex-col container self-center h-11/12 w-full">
         <img src="../../stylesheets/images/logo.png" class="h-96 w-96 mx-auto">
-        <button class="hover:bg-orange-default hover:text-dark-default border-2 border-orange-default font-bold text-center py-2 px-6 w-1/3 mx-auto relative" type="button">
-        <router-link
-          to="/titles"
-        >はじめる</router-link>
-        <span class="animate-ping absolute top-0 left-0 h-full w-full bg-orange-default opacity-75"></span>
-        </button>
+          <button class="font-bold text-center py-2 px-6 w-44 mx-auto z-10" type="button">
+          <router-link
+            to="/titles"
+          >はじめる</router-link>
+          </button>
+          <div class="w-44 h-4 mx-auto relative">
+            <span class="animate-ping absolute -top-8 left-0 h-6 w-44 bg-orange-default opacity-75 rounded"></span>
+          </div>
       </div>
     </div>
   </div>
